b1e54dd3240bbe527d67f90ccf76cb8e17d8dda8
falcon <hayato.fujita@furukawaelectric.com>
2018-04-09 22:23:41 +0900

    fft

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import numpy as np
-test = 'test'
+
+time_start = 0
+time_end = 1
+sampling = 0.001
+freq = 1000
+
+time = np.arange(time_start,time_end,sampling)
+x = np.sin(np.pi * freq * time)
+y = np.cos(np.pi * freq * time)
+sig = x + y * 1j
+sig_fft = np.fft.fft(sig)
+
