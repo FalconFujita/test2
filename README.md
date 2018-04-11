391a1ebe54bae88d61adecf7c9b3b84c4f8cf51e
falcon <hayato.fujita@furukawaelectric.com>
2018-04-12 00:24:11 +0900

    add sig function

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ test how to use github for me
 ## Updata
 $ git clone
 
-$ git add -A
-$ git commit -m"text"
-$ git push origin master
+$ git add -A  
+$ git commit -m"text"  
+$ git push origin master  
 
 ## Requirement
 
--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import numpy as np
 
-time_start = 0
-time_end = 1
-sampling = 0.001
-freq = 1000
+def sig(amp,freq):
+    time_start = 0
+    time_end = 1
+    sampling = 0.001
+    
+    time = np.arange(time_start,time_end,sampling)
+    x = amp * np.sin(np.pi * freq * time)
+    y = amp * np.cos(np.pi * freq * time)
+    sig = x + y * 1j
+    sig_fft = np.fft.fft(sig)
+    sig_fft_max = sig_fft.max()
+    print(sig_fft_max)
 
-time = np.arange(time_start,time_end,sampling)
-x = np.sin(np.pi * freq * time)
-y = np.cos(np.pi * freq * time)
-sig = x + y * 1j
-sig_fft = np.fft.fft(sig)
-
+if __name__ == '__main__':
+    sig(10,1000)
